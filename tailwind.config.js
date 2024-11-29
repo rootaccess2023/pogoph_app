7b8c3ea452d40b072ed2e44f111416520301ff74
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-29 23:19:37 +0100

    tailwind: extend colors with custom mapsMenu palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        mapsMenu: {
+          primary: "#ec4747",
+          tertiary: "#70757a",
+        },
+      },
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
+        sans: ["Roboto", "sans-serif"],
       },
     },
   },
