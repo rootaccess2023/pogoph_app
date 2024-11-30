bde6bbcf689c852f4f1c99459e88e4ce2d142499
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-30 21:38:37 +0100

    feat: implement setZoom, setPitch, and setSelectedPogo in PogoList component

--- src/components/MapBox/PogoList.tsx
+++ src/components/MapBox/PogoList.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useMenuStore, usePogoStore } from "../../stores";
+import { Pogo, useMenuStore, usePogoStore, useViewStore } from "../../stores";
 import { MdShareLocation } from "react-icons/md";
 
 export function PogoList() {
@@ -7,7 +7,19 @@ export function PogoList() {
   const year = usePogoStore((state) => state.year);
   const filteredPogo = usePogoStore((state) => state.filteredPogo);
   const setPogo = usePogoStore((state) => state.setPogo);
+  const setZoom = useViewStore((state) => state.setZoom);
+  const setPitch = useViewStore((state) => state.setPitch);
   const setFilteredPogo = usePogoStore((state) => state.setFilteredPogo);
+  const setSelectedPogo = usePogoStore((state) => state.setSelectedPogo);
+
+  const handleSelectedPogo = (location: Pogo) => {
+    setSelectedPogo({
+      latitude: location.latitude,
+      longitude: location.longitude,
+    });
+    setPitch(60);
+    setZoom(18);
+  };
 
   useEffect(() => {
     const fetchLocations = async () => {
@@ -40,6 +52,7 @@ export function PogoList() {
       <ul className="h-[290px] w-[376px] bg-white rounded-bl-3xl rounded-br-3xl py-2 overflow-y-scroll">
         {filteredPogo.map((location, index) => (
           <li
+            onClick={() => handleSelectedPogo(location)}
             key={index}
             className="flex items-center px-5 gap-5 hover:bg-mapsMenu-shadow cursor-pointer"
           >
