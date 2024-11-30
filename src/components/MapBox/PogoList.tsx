cc1970fff1fdb04489b1969364f7165f4061f132
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-30 18:32:41 +0100

    feat: integrate filteredPogo state into PogoList for refined location display
    - Added `setFilteredPogo` to initialize filtered locations after fetching data.
    - Updated `useEffect` to set both `pogo` and `filteredPogo` states upon fetching.
    - Modified the list to render `filteredPogo`, ensuring compatibility with search filters.
    - Retained error handling for robust API interaction.

--- src/components/MapBox/PogoList.tsx
+++ src/components/MapBox/PogoList.tsx
@@ -5,8 +5,9 @@ import { MdShareLocation } from "react-icons/md";
 export function PogoList() {
   const isOpen = useMenuStore((state) => state.isOpen);
   const year = usePogoStore((state) => state.year);
-  const pogo = usePogoStore((state) => state.pogo);
+  const filteredPogo = usePogoStore((state) => state.filteredPogo);
   const setPogo = usePogoStore((state) => state.setPogo);
+  const setFilteredPogo = usePogoStore((state) => state.setFilteredPogo);
 
   useEffect(() => {
     const fetchLocations = async () => {
@@ -18,7 +19,9 @@ export function PogoList() {
           throw new Error(`Error: ${response.statusText}`);
         }
         const data = await response.json();
+
         setPogo(data);
+        setFilteredPogo(data);
       } catch (error) {
         console.error("Failed to fetch locations:", error);
         setPogo([]);
@@ -35,7 +38,7 @@ export function PogoList() {
       }`}
     >
       <ul className="h-[290px] w-[376px] bg-white rounded-bl-3xl rounded-br-3xl py-2 overflow-y-scroll">
-        {pogo.map((location, index) => (
+        {filteredPogo.map((location, index) => (
           <li
             key={index}
             className="flex items-center px-5 gap-5 hover:bg-mapsMenu-shadow cursor-pointer"
