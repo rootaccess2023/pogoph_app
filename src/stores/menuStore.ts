75312747e5b041ad17c83abf560657eef1b2055b
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-30 23:33:16 +0100

    feat(menuStore): add menuOpen state and setMenuOpen action

--- src/stores/menuStore.ts
+++ src/stores/menuStore.ts
@@ -4,10 +4,14 @@ interface MenuStore {
   isOpen: boolean;
   setOpen: () => void;
   setClose: () => void;
+  menuOpen: boolean;
+  setMenuOpen: () => void;
 }
 
 export const useMenuStore = create<MenuStore>((set) => ({
   isOpen: false,
   setOpen: () => set({ isOpen: true }),
   setClose: () => set({ isOpen: false }),
+  menuOpen: false,
+  setMenuOpen: () => set({ menuOpen: true }),
 }));
