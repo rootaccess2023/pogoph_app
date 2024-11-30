d005f53c07427cfcc543de81a24dc891d5c48b4d
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-30 21:37:57 +0100

    feat: add selectedPogo and setSelectedPogo to PogoStore and export Pogo interface

--- src/stores/pogoStore.ts
+++ src/stores/pogoStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-interface Pogo {
+export interface Pogo {
   name?: string;
   latitude: number;
   longitude: number;
@@ -15,16 +15,20 @@ interface PogoStore {
   year: number;
   pogo: Pogo[];
   filteredPogo: Pogo[];
+  selectedPogo: Pogo | null;
   setYear: (year: number) => void;
   setPogo: (pogo: Pogo[]) => void;
   setFilteredPogo: (pogo: Pogo[]) => void;
+  setSelectedPogo: (pogo: Pogo | null) => void;
 }
 
 export const usePogoStore = create<PogoStore>((set) => ({
   year: 2017,
   pogo: [],
   filteredPogo: [],
+  selectedPogo: null,
   setYear: (year: number) => set({ year: year }),
   setPogo: (data: Pogo[]) => set({ pogo: data }),
   setFilteredPogo: (data: Pogo[]) => set({ filteredPogo: data }),
+  setSelectedPogo: (pogo: Pogo | null) => set({ selectedPogo: pogo }),
 }));
