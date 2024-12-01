10246f5484e3c5d8ffc821a0ff7cb6bf07d2b26a
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-12-01 10:38:34 +0100

    feat(MapBox): implement sidebar toggle functionality with menuOpen and menuClose

--- src/layout/MapBox.tsx
+++ src/layout/MapBox.tsx
@@ -3,12 +3,16 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { Details, MarkerList, PogoList, SearchBar } from "../components";
 import { useMenuStore, usePogoStore, useViewStore } from "../stores";
 import { useEffect, useRef } from "react";
+import { FaCaretRight } from "react-icons/fa";
 
 export function MapBox() {
   const zoom = useViewStore((state) => state.zoom);
   const pitch = useViewStore((state) => state.pitch);
   const selectedPogo = usePogoStore((state) => state.selectedPogo);
   const menuOpen = useMenuStore((state) => state.menuOpen);
+  const setMenuClose = useMenuStore((state) => state.setMenuClose);
+  const setMenuOpen = useMenuStore((state) => state.setMenuOpen);
+
   const MAPBOX_TOKEN =
     "pk.eyJ1IjoicG9sb3JldmlsbzE5IiwiYSI6ImNtM2xydzVyZTByZ2Qyc3BlZ2gwaGcwZnEifQ.pQZoqD9Md_xq2JAlYZzoLw";
   const mapRef = useRef<MapRef>(null);
@@ -52,6 +56,27 @@ export function MapBox() {
         } absolute z-10 top-0 left-0 w-[408px] h-screen bg-white`}
       >
         <Details />
+      </div>
+      <div
+        className={`absolute h-screen ${
+          menuOpen === true ? "left-[408px]" : "left-0"
+        } top-0 flex items-center`}
+      >
+        {menuOpen === true ? (
+          <span
+            onClick={() => setMenuClose()}
+            className="z-20 h-8 flex items-center bg-white rounded-tr-md rounded-br-md drop-shadow"
+          >
+            <FaCaretRight className="relative right-[2px] size-5 rotate-180" />
+          </span>
+        ) : (
+          <span
+            onClick={() => setMenuOpen()}
+            className="z-20 h-8 flex items-center bg-white rounded-tr-md rounded-br-md drop-shadow"
+          >
+            <FaCaretRight className="size-5" />
+          </span>
+        )}
       </div>
     </div>
   );
