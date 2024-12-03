c558647991b8e9edf9bab63c2a63cd4a203d9e92
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-12-03 22:01:49 +0100

    feat(AppContainer): integrate timelineOpen state to AppContainer

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,9 +1,9 @@
-import { MapBox } from "./layout";
+import { MapBox, Timeline } from "./layout";
+import { useMenuStore } from "./stores";
 
 export function AppContainer() {
+  const timelineOpen = useMenuStore((state) => state.timelineOpen);
   return (
-    <div className="">
-      <MapBox />
-    </div>
+    <div className="">{timelineOpen === false ? <MapBox /> : <Timeline />}</div>
   );
 }
