31bb8e0af7a89dae41348d9e6d7891efb5d9e2cd
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-12-03 22:30:35 +0100

    feat(store): add timelineOpena nd setTimelineOpen function

--- src/stores/menuStore.ts
+++ src/stores/menuStore.ts
@@ -6,10 +6,12 @@ interface MenuStore {
   setClose: () => void;
   menuOpen: boolean;
   sideOpen: boolean;
+  timelineOpen: boolean;
   setMenuOpen: () => void;
   setMenuClose: () => void;
   setSideOpen: () => void;
   setSideClose: () => void;
+  toggleTimelineOpen: () => void;
 }
 
 export const useMenuStore = create<MenuStore>((set) => ({
@@ -18,8 +20,11 @@ export const useMenuStore = create<MenuStore>((set) => ({
   setClose: () => set({ isOpen: false }),
   menuOpen: false,
   sideOpen: false,
+  timelineOpen: false,
   setMenuOpen: () => set({ menuOpen: true }),
   setMenuClose: () => set({ menuOpen: false }),
   setSideOpen: () => set({ sideOpen: true }),
   setSideClose: () => set({ sideOpen: false }),
+  toggleTimelineOpen: () =>
+    set((state) => ({ timelineOpen: !state.timelineOpen })),
 }));
